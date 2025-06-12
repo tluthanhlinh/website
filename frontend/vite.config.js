fe00a8995baa12ec59c8d2e567a5533462d80d8d
Phan Xuan Tien <xuantiengitlab.cntt@gmail.com>
2025-06-12 15:13:49 +0700

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// D:\websiteTLU\frontend\vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/', // Rất quan trọng cho Vercel/Netlify để tạo đường dẫn asset đúng
+  build: {
+    outDir: 'dist', // Thư mục output mặc định của Vite
+  },
+});
